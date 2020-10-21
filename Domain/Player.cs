9d6ca8ec521f059bc3e8590fc2f46e4c5722f2df
tiagosada <tiagocostasada@gmail.com>
2020-10-21 17:07:20 -0300

    using herança

--- Domain/Player.cs
+++ Domain/Player.cs
@@ -1,4 +1,10 @@
 using System;
+<<<<<<< HEAD
+=======
+using System.Collections.Generic;
+using System.Linq;
+
+>>>>>>> ccf8f61... adding PlayerTests
 namespace Domain
 {
     public class Player
@@ -6,12 +12,48 @@ namespace Domain
         public Guid Id { get; private set; } = Guid.NewGuid();
         public string Name { get; private set; }
 
+<<<<<<< HEAD
         //public int Goals { get; set; }
         //public string Position { get; set; }
 
         public Player(string name)
+=======
+        public Player(string name, int goals, int position)
+        {
+            this.Id = Guid.NewGuid();
+
+            this.Goals = goals;
+
+            this.Position = position;
+
+            this.Name = name;
+
+            if (!this.ValidateName())
+            {
+                this.Name = null;
+            }
+        }
+        public string PlayerPosition( int position)
+>>>>>>> ccf8f61... adding PlayerTests
         {
             Name = name;
+        }
+        public bool ValidateName()
+        {
+            if (string.IsNullOrEmpty(this.Name))
+            {
+                return false;
+            }
+            if (!Name.All(x => char.IsLetter(x) || char.IsWhiteSpace(x)))
+            {
+                return false;
+            }
+            if (!char.IsUpper(Name[0]))
+            {
+                return false;
+            }
+            return true;
+            
         }
     }
 }
--- Tests/PlayerTests.cs
+++ Tests/PlayerTests.cs
@@ -6,6 +6,7 @@ namespace Tests
 {
     public class PlayerTest
     {
+<<<<<<< HEAD
         [Fact]
         public void Should_contains_Player_Name()
         {
@@ -14,6 +15,29 @@ namespace Tests
 
            // Assert.Equal(name, player.Name);
             Assert.Equal(name, player.Name);
+=======
+        [Theory]
+        [InlineData("Joao", 1, 2)]
+        public void Should_contains_Player_validname_position_and_goals(string name, int position, int goals)
+        {
+            var player = new Player(name, goals, position);
+
+           // Assert.Equal(name, player.Name);
+            Assert.True(player.ValidateName());
+            Assert.Equal(goals, player.Goals);
+            Assert.Equal(position, player.Position);
+        }
+        [Theory]
+        [InlineData("", 0, 0)]
+        public void Should_retuns_false_when_passed_a_invalid_name(string name, int position, int goals)
+        {
+            var player = new Player(name, goals, position);
+
+           // Assert.Equal(name, player.Name);
+            Assert.False(player.ValidateName());
+            Assert.Equal(goals, player.Goals);
+            Assert.Equal(position, player.Position);
+>>>>>>> ccf8f61... adding PlayerTests
         }
         
     }
